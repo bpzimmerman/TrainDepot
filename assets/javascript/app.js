9c02c946dd68feb3823e412aecb59b9aa0f9b108
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-01-29 10:07:29 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// initialize firebase
+var config = {
+    apiKey: "AIzaSyAGnxQ5UGj4OjYcJP57uCarhTA2BHVm7sM",
+    authDomain: "fir-sucks-4a8f6.firebaseapp.com",
+    databaseURL: "https://fir-sucks-4a8f6.firebaseio.com",
+    projectId: "fir-sucks-4a8f6",
+    storageBucket: "",
+    messagingSenderId: "285162773561"
+  };
+
+  firebase.initializeApp(config);
