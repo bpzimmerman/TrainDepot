9cb03ca23a0911723662710a519dfa264fbf49f0
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-01-29 11:06:42 -0500

    created form and added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,15 @@
-// initialize firebase
-var config = {
-    apiKey: "AIzaSyAGnxQ5UGj4OjYcJP57uCarhTA2BHVm7sM",
-    authDomain: "fir-sucks-4a8f6.firebaseapp.com",
-    databaseURL: "https://fir-sucks-4a8f6.firebaseio.com",
-    projectId: "fir-sucks-4a8f6",
-    storageBucket: "",
-    messagingSenderId: "285162773561"
-  };
+$(document).ready(function(){
+    // initialize firebase
+    var config = {
+        apiKey: "AIzaSyAGnxQ5UGj4OjYcJP57uCarhTA2BHVm7sM",
+        authDomain: "fir-sucks-4a8f6.firebaseapp.com",
+        databaseURL: "https://fir-sucks-4a8f6.firebaseio.com",
+        projectId: "fir-sucks-4a8f6",
+        storageBucket: "",
+        messagingSenderId: "285162773561"
+    };
 
-  firebase.initializeApp(config);
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+});
