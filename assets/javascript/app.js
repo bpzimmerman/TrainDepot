5528fd9dab6e43249a4661fcc0b2c2df2282764a
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-01-30 18:05:27 -0500

    added functionality to update the minutes to arrival every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,22 +11,71 @@ $(document).ready(function(){
 
     firebase.initializeApp(config);
 
-    // declare variables
+    // declare the database variable
     var database = firebase.database();
 
-    var trainName = "";
-    var destination = "";
-    var firstTrainTime = "";
-    var frequency = 0;
+    // declare the 
+    // var trainName = "";
+    // var destination = "";
+    // var firstTrainTime = "";
+    // var frequency = 0;
+
+    // declare the variables for the interval timer
+    var intervalId;
+    var minutes = moment().format("mm");
+
+    // function to build the train data table with the information in the database
+    function trainInfo(){
+        // event listener that runs when data is added to the firebase database
+        database.ref().on("child_added", function(shot){
+            // gets the child data from the database and assigns it to variables
+            var train = shot.val().train;
+            var dest = shot.val().dest;
+            var time = shot.val().time;
+            var freq = shot.val().freq;
+
+            // performs the time calculations to get how many minutes away
+            var timeConverted = moment(time, "hh:mm").subtract(1, "years");
+            var timeDifference = moment().diff(moment(timeConverted), "minutes");
+            var remainder = timeDifference % freq;
+            var minutesAway = freq - remainder;
+            var nextArrival = moment().add(minutesAway, "minutes");
+
+            // creates the rows and items that will go into the table body
+            var tableRow = $("<tr>");
+            tableRow.attr("id", shot.key);
+            var trainItem = $("<td>");
+            trainItem.text(train);
+            var destItem = $("<td>");
+            destItem.text(dest);
+            var freqItem = $("<td>");
+            freqItem.text(freq);
+            var min = $("<td>");
+            min.text(minutesAway);
+            var next = $("<td>");
+            next.text(moment(nextArrival).format("hh:mm A"));
+            // creates buttons to remove trains from the list
+            var rem = $("<button>");
+            rem.text("X");
+            rem.attr("class", "btn btn-default remove");
+            rem.attr("data-key", shot.key);
+
+            // builds the complete row item and prepends it to the table
+            tableRow.append(trainItem, destItem, freqItem, next, min, rem);
+            $("#trainData").prepend(tableRow);
+        }, function(errorObject){
+            console.log("Errors handled: " + errorObject.code);
+        });
+    };    
 
     // event listener that runs when the submit button is clicked 
     $("#submit-button").on("click", function(event){
         event.preventDefault();
         // gets the values from the form and assigns them to variables
-        trainName = $("#train-input").val().trim();
-        destination = $("#dest-input").val().trim();
-        firstTrainTime = $("#time-input").val().trim();
-        frequency = parseInt($("#freq-input").val().trim());
+        var trainName = $("#train-input").val().trim();
+        var destination = $("#dest-input").val().trim();
+        var firstTrainTime = $("#time-input").val().trim();
+        var frequency = parseInt($("#freq-input").val().trim());
         // clears the form
         $("#train-input").val("");
         $("#dest-input").val("");
@@ -39,47 +88,9 @@ $(document).ready(function(){
             time: firstTrainTime,
             freq: frequency
         });
-    });
-
-    // event listener that runs when data is added to the firebase database
-    database.ref().on("child_added", function(shot){
-        // gets the child data from the database and assigns it to variables
-        var train = shot.val().train;
-        var dest = shot.val().dest;
-        var time = shot.val().time;
-        var freq = shot.val().freq;
-
-        // performs the time calculations to get how many minutes away
-        var timeConverted = moment(time, "hh:mm").subtract(1, "years");
-        var timeDifference = moment().diff(moment(timeConverted), "minutes");
-        var remainder = timeDifference % freq;
-        var minutesAway = freq - remainder;
-        var nextArrival = moment().add(minutesAway, "minutes");
-
-        // creates the rows and items that will go into the table body
-        var tableRow = $("<tr>");
-        tableRow.attr("id", shot.key);
-        var trainItem = $("<td>");
-        trainItem.text(train);
-        var destItem = $("<td>");
-        destItem.text(dest);
-        var freqItem = $("<td>");
-        freqItem.text(freq);
-        var min = $("<td>");
-        min.text(minutesAway);
-        var next = $("<td>");
-        next.text(moment(nextArrival).format("hh:mm A"));
-        // creates buttons to remove trains from the list
-        var rem = $("<button>");
-        rem.text("X");
-        rem.attr("class", "btn btn-default remove");
-        rem.attr("data-key", shot.key);
-
-        // builds the complete row item and prepends it to the table
-        tableRow.append(trainItem, destItem, freqItem, next, min, rem);
-        $("#trainData").prepend(tableRow);
-    }, function(errorObject){
-        console.log("Errors handled: " + errorObject.code);
+        // empty the train data table and call the function to rebuild the table with the updated information
+        $("#trainData").empty();
+        trainInfo();
     });
 
     // event listener to remove a train from the database and the list
@@ -88,4 +99,16 @@ $(document).ready(function(){
         database.ref().child(k).remove();
         $("#" + k).empty();
     });
+
+    // interval to change the next arrival time in the train data table
+    intervalId = setInterval(function(){
+        if (moment().format("mm") != minutes){
+            $("#trainData").empty();
+            trainInfo();
+            minutes = moment().format("mm");
+        };
+    }, 1000 * 5);
+
+    // initial function call to build the data table with the information in the database
+    trainInfo();
 });
